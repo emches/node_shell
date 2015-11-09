08b20ef64f72a18edde633c31a9ad1ab4399d5a8
ajpz <perez.jonthan@gmail.com>
2015-11-09 16:51:07 -0500

    add wc, uniq, curl, sort and refactor with done function

--- bash.js
+++ bash.js
@@ -1,15 +1,15 @@
 var command = require('./commands.js');
 
+var done = function(output) {
+   process.stdout.write(output);
+   process.stdout.write('\nprompt > ');
+}; 
+
 // Output a prompt
 process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
-   var cmd = data.toString().trim().split(" ")[0]; // remove the newline
-
-   var args = data.toString().trim().split(" ").slice(1)
-
-      command[cmd](args);
-      process.stdout.write('\nprompt > ');
-
-
+	var cmd = data.toString().trim().split(" ")[0]; // remove the newline
+	var args = data.toString().trim().split(" ").slice(1)
+	command[cmd](args, done);
 });
--- commands.js
+++ commands.js
@@ -1,51 +1,70 @@
 var fs = require('fs');
+var request = require('request'); 
+
+
 module.exports = {
-  pwd: function(input) {
-    process.stdout.write(process.cwd());
-    process.stdout.write('\nprompt > ');
-
+  pwd: function(input, done) {
+    done(process.cwd()); 
   },
-  date: function(input) {
-   process.stdout.write(new Date().toString());
-   process.stdout.write('\nprompt > ');
-
+  date: function(input, done) {
+    done(new Date().toString());
   },
-  ls: function(input) {
-
-    var files = fs.readdirSync('.');
-    files.forEach(function(file) {
-      process.stdout.write(file.toString() + "\n");
-     });
-    process.stdout.write('\nprompt > ');
-
+  ls: function(input, done) {
+    var output = fs.readdirSync('.').toString().split(',').join('\n');
+    done(output);
    },
-   echo: function(input){
-    process.stdout.write(input.join(" "));
-    process.stdout.write('\nprompt > ');
-
+   echo: function(input, done){
+    done(input.join(' '));
    },
-   cat: function(input){
+   cat: function(input, done){
     fs.readFile("./"+input, function(err, file){
-       process.stdout.write(file.toString());
-       process.stdout.write('\nprompt > ');
-
+        done(file.toString());
     });
    },
-   head: function(input){
+   head: function(input, done){
     fs.readFile("./"+input, function(err, file){
-       process.stdout.write(file.toString().split("\n").splice(0,5).join("\n"));
-       process.stdout.write('\nprompt > ');
-
+       done(file.toString().split("\n").splice(0,5).join("\n"));
     });
    },
 
-   tail: function(input){
+   tail: function(input, done){
     fs.readFile("./"+input, function(err, file){
-       process.stdout.write(file.toString().split("\n").splice(-5).join("\n"));
-       process.stdout.write('\nprompt > ');
-
+       done(file.toString().split("\n").splice(-5).join("\n"));
     });
-
-   }
+   },
+   sort: function(input, done) {
+    fs.readFile('./'+input, function(err, file) {
+        done(file.toString().split("\n").sort().join('\n'));
+    })
+   },
+   wc: function(input, done) {
+    fs.readFile('./'+input, function(err, file) {
+        var arr = file.toString().split("\n").length;
+        // console.log(typeof arr === 'object');  
+        // console.log(arr); 
+        process.stdout.write(arr);
+        // process.stdout.write(arr.length);
+        process.stdout.write('\nprompt > ');        
+        done(output);
+    })
+   },
+   uniq: function(input, done) {
+    fs.readFile('./'+input, function(err, file) {
+        var cleanArr = []; 
+        var arr = file.toString().split("\n"); 
+        arr.forEach(function(line) {
+            if(cleanArr.indexOf(line) === -1) cleanArr.push(line); 
+        }); 
+        done(cleanArr.join('\n'));
+    }) 
+        
+    },
+    curl: function(input, done) {
+        request(input[0], function(error, response, body) {
+            if(!error && response.statusCode == 200) {
+                done(body)
+            }
+        })
+    }
 
 }
