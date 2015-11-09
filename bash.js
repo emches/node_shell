6ec06e9eba2285ce8e7cd8ea7c2423fc3686fa30
emilyches <emilychesler@gmail.com>
2015-11-09 18:12:07 -0500

    added stdin for pipes

--- bash.js
+++ bash.js
@@ -1,15 +1,22 @@
 var command = require('./commands.js');
 
-var done = function(output) {
-   process.stdout.write(output);
+var done = function(stdin, output) {
+   if ( stdin.length ===0) {process.stdout.write(output);
    process.stdout.write('\nprompt > ');
-}; 
+   } else {
+    nextFunc = stdin[0]
+
+    stdin = stdin.slice(1);
+    command[nextFunc](stdin, output, done);
+   }
+};
 
 // Output a prompt
 process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
 	var cmd = data.toString().trim().split(" ")[0]; // remove the newline
-	var args = data.toString().trim().split(" ").slice(1)
-	command[cmd](args, done);
+	var input = data.toString().trim().split(" ").slice(1)[0];
+    var stdin = data.toString().trim().split(/\s*\|\s*/g).slice(1)
+	command[cmd](stdin, input, done);
 });
--- commands.js
+++ commands.js
@@ -1,66 +1,68 @@
 var fs = require('fs');
-var request = require('request'); 
+var request = require('request');
 
 
 module.exports = {
-  pwd: function(input, done) {
-    done(process.cwd()); 
+  pwd: function(stdin, input, done) {
+    done(process.cwd());
   },
-  date: function(input, done) {
+  date: function(stdin, input, done) {
     done(new Date().toString());
   },
-  ls: function(input, done) {
+  ls: function(stdin, input, done) {
     var output = fs.readdirSync('.').toString().split(',').join('\n');
     done(output);
    },
-   echo: function(input, done){
+   echo: function(stdin, input, done){
     done(input.join(' '));
    },
-   cat: function(input, done){
+   cat: function(stdin, input, done){
+
     fs.readFile("./"+input, function(err, file){
-        done(file.toString());
+
+            done(stdin, file.toString());
     });
    },
-   head: function(input, done){
-    fs.readFile("./"+input, function(err, file){
-       done(file.toString().split("\n").splice(0,5).join("\n"));
-    });
+   head: function(stdin, input, done){
+    if (arguments.callee.caller===done){
+       done(stdin, input.toString().split("\n").splice(0,5).join("\n"));
+   } else {
+     fs.readFile("./"+input, function(err, file){
+       done(stdin, file.toString().split("\n").splice(0,5).join("\n"));
+     });
+   }
+
    },
 
-   tail: function(input, done){
+   tail: function(stdin, input, done){
     fs.readFile("./"+input, function(err, file){
        done(file.toString().split("\n").splice(-5).join("\n"));
     });
    },
-   sort: function(input, done) {
+   sort: function(stdin, input, done) {
     fs.readFile('./'+input, function(err, file) {
         done(file.toString().split("\n").sort().join('\n'));
     })
    },
-   wc: function(input, done) {
+   wc: function(stdin, input, done) {
     fs.readFile('./'+input, function(err, file) {
-        var arr = file.toString().split("\n").length;
-        // console.log(typeof arr === 'object');  
-        // console.log(arr); 
-        process.stdout.write(arr);
-        // process.stdout.write(arr.length);
-        process.stdout.write('\nprompt > ');        
-        done(output);
+
+        done(stdin, file.toString().split("\n").length.toString());
     })
    },
-   uniq: function(input, done) {
+   uniq: function(stdin, input, done) {
     fs.readFile('./'+input, function(err, file) {
-        var cleanArr = []; 
-        var arr = file.toString().split("\n"); 
+        var cleanArr = [];
+        var arr = file.toString().split("\n");
         arr.forEach(function(line) {
-            if(cleanArr.indexOf(line) === -1) cleanArr.push(line); 
-        }); 
+            if(cleanArr.indexOf(line) === -1) cleanArr.push(line);
+        });
         done(cleanArr.join('\n'));
-    }) 
-        
+    })
+
     },
-    curl: function(input, done) {
-        request(input[0], function(error, response, body) {
+    curl: function(stdin, input, done) {
+        request(input, function(error, response, body) {
             if(!error && response.statusCode == 200) {
                 done(body)
             }
