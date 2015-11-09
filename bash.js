074a9177bac011346adb24ab24cafa91670e5fd0
emilyches <emilychesler@gmail.com>
2015-11-09 15:46:21 -0500

    cat bash and tail work

--- bash.js
+++ bash.js
@@ -1,14 +1,15 @@
-var command = require('./commands.js')
+var command = require('./commands.js');
 
 // Output a prompt
 process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
-   var cmd = data.toString().trim(); // remove the newline
+   var cmd = data.toString().trim().split(" ")[0]; // remove the newline
 
+   var args = data.toString().trim().split(" ").slice(1)
 
-   command[cmd]();
+      command[cmd](args);
+      process.stdout.write('\nprompt > ');
 
-  process.stdout.write('\nprompt > ');
 
 });
--- commands.js
+++ commands.js
@@ -1,8 +1,51 @@
+var fs = require('fs');
 module.exports = {
-  pwd: function() {
+  pwd: function(input) {
     process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
+
   },
-  date: function() {
+  date: function(input) {
    process.stdout.write(new Date().toString());
-  }
+   process.stdout.write('\nprompt > ');
+
+  },
+  ls: function(input) {
+
+    var files = fs.readdirSync('.');
+    files.forEach(function(file) {
+      process.stdout.write(file.toString() + "\n");
+     });
+    process.stdout.write('\nprompt > ');
+
+   },
+   echo: function(input){
+    process.stdout.write(input.join(" "));
+    process.stdout.write('\nprompt > ');
+
+   },
+   cat: function(input){
+    fs.readFile("./"+input, function(err, file){
+       process.stdout.write(file.toString());
+       process.stdout.write('\nprompt > ');
+
+    });
+   },
+   head: function(input){
+    fs.readFile("./"+input, function(err, file){
+       process.stdout.write(file.toString().split("\n").splice(0,5).join("\n"));
+       process.stdout.write('\nprompt > ');
+
+    });
+   },
+
+   tail: function(input){
+    fs.readFile("./"+input, function(err, file){
+       process.stdout.write(file.toString().split("\n").splice(-5).join("\n"));
+       process.stdout.write('\nprompt > ');
+
+    });
+
+   }
+
 }
